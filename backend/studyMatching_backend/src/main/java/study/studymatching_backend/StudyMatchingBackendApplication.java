c948d8a0d47b53afba1b2f602baf976973596e5d
SunghoCha <tjdgh3725@gmail.com>
2024-08-21 23:42:20 +0900

    studyMatching - setting commit2

---
+++ backend/studyMatching_backend/src/main/java/study/studymatching_backend/StudyMatchingBackendApplication.java
@@ -0,0 +1,13 @@
+package study.studymatching_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMatchingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyMatchingBackendApplication.class, args);
+    }
+
+}
